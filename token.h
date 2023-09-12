0837304d810032f53900a9651bb1a96761cb5cc3
Ziang Tong <ztong2@student10.cse.nd.edu>
2023-09-12 11:43:05 -0400

    Scanner - add sectional comments for readability

--- token.h
+++ token.h
@@ -1,7 +1,11 @@
 #ifndef TOKEN_H
 #define TOKEN_H
 
+/* Identifier and assignment */
 #define TOKEN_IDENTIFIER
+#define TOKEN_ASGMT
+
+/* Type keywords and literals */
 #define TOKEN_INT       
 #define TOKEN_FLOAT     
 #define TOKEN_STR       
@@ -11,24 +15,29 @@
 #define TOKEN_FLOAT_LIT 
 #define TOKEN_STR_LIT   
 #define TOKEN_CHAR_LIT  
+
+/* Groups and scopes */
 #define TOKEN_LPAREN
 #define TOKEN_RPAREN
 #define TOKEN_LBRACKET
 #define TOKEN_RBRACKET
 #define TOKEN_FLPAREN
-#define TOKEN_INCRE
-#define TOKEN_DECRE
+
+/* Logical operation */
 #define TOKEN_NEG
 #define TOKEN_NOT
 #define TOKEN_AND
 #define TOKEN_OR
+
+/* Math operation */
 #define TOKEN_EXP
 #define TOKEN_MULT
 #define TOKEN_DIV
 #define TOKEN_REM
 #define TOKEN_ADD
 #define TOKEN_SUB
+#define TOKEN_INCRE
+#define TOKEN_DECRE
 #define TOKEN_COMP
-#define TOKEN_ASGMT
 
 #endif
