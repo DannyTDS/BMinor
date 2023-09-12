f949df63866aabd2d1ce519d4f478e01c9931acd
Ziang Tong <ztong2@student10.cse.nd.edu>
2023-09-12 11:37:58 -0400

    Scanner - add definition of tokens

---
+++ token.h
@@ -0,0 +1,34 @@
+#ifndef TOKEN_H
+#define TOKEN_H
+
+#define TOKEN_IDENTIFIER
+#define TOKEN_INT       
+#define TOKEN_FLOAT     
+#define TOKEN_STR       
+#define TOKEN_CHAR      
+#define TOKEN_BOOL      
+#define TOKEN_INT_LIT   
+#define TOKEN_FLOAT_LIT 
+#define TOKEN_STR_LIT   
+#define TOKEN_CHAR_LIT  
+#define TOKEN_LPAREN
+#define TOKEN_RPAREN
+#define TOKEN_LBRACKET
+#define TOKEN_RBRACKET
+#define TOKEN_FLPAREN
+#define TOKEN_INCRE
+#define TOKEN_DECRE
+#define TOKEN_NEG
+#define TOKEN_NOT
+#define TOKEN_AND
+#define TOKEN_OR
+#define TOKEN_EXP
+#define TOKEN_MULT
+#define TOKEN_DIV
+#define TOKEN_REM
+#define TOKEN_ADD
+#define TOKEN_SUB
+#define TOKEN_COMP
+#define TOKEN_ASGMT
+
+#endif
